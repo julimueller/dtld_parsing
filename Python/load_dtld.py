2884710d326ee83eb6ff02cbbcb1d868b32f29b2
Julian Mueller <muelju3@cmtcde8u013.rd.corpintra.net>
2020-03-25 21:14:16 +0100

    indents, add comments

--- Python/load_dtld.py
+++ Python/load_dtld.py
@@ -46,38 +46,39 @@ def parse_args():
 
 def main(args):
 
-  # Load database
-  database = DriveuDatabase(args.label_file)
-  database.open(args.data_base_dir)
+    # Load database
+    database = DriveuDatabase(args.label_file)
+    database.open(args.data_base_dir)
 
-  # Load calibration
-  calibration = CalibrationData()
-  intrinsic_left = calibration.load_intrinsic_matrix(args.calib_dir + '/intrinsic_left.yml')
-  rectification_left = calibration.load_rectification_matrix(args.calib_dir + '/rectification_left.yml')
-  projection_left = calibration.load_projection_matrix(args.calib_dir + '/projection_left.yml')
-  extrinsic = calibration.load_extrinsic_matrix(args.calib_dir + '/extrinsic.yml')
-  distortion_left = calibration.load_distortion_matrix(args.calib_dir + '/distortion_left.yml')
+    # Load calibration
+    calibration = CalibrationData()
+    intrinsic_left = calibration.load_intrinsic_matrix(args.calib_dir + '/intrinsic_left.yml')
+    rectification_left = calibration.load_rectification_matrix(args.calib_dir + '/rectification_left.yml')
+    projection_left = calibration.load_projection_matrix(args.calib_dir + '/projection_left.yml')
+    extrinsic = calibration.load_extrinsic_matrix(args.calib_dir + '/extrinsic.yml')
+    distortion_left = calibration.load_distortion_matrix(args.calib_dir + '/distortion_left.yml')
 
-  logging.info("Intrinsic Matrix:\n\n{}\n".format(intrinsic_left))
-  logging.info("Extrinsic Matrix:\n\n{}\n".format(extrinsic))
-  logging.info("Projection Matrix:\n\n{}\n".format(projection_left))
-  logging.info("Rectification Matrix:\n\n{}\n".format(rectification_left))
-  logging.info("Distortion Matrix:\n\n{}\n".format(distortion_left))
+    logging.info("Intrinsic Matrix:\n\n{}\n".format(intrinsic_left))
+    logging.info("Extrinsic Matrix:\n\n{}\n".format(extrinsic))
+    logging.info("Projection Matrix:\n\n{}\n".format(projection_left))
+    logging.info("Rectification Matrix:\n\n{}\n".format(rectification_left))
+    logging.info("Distortion Matrix:\n\n{}\n".format(distortion_left))
 
-  for idx_d, img in enumerate(database.images):
-
-    img_disp = img.visualize_disparity_image()
-    rects = img.map_labels_to_disparity_image(calibration)
-
-    for rect in rects:
-      cv2.rectangle(img_disp, (int(rect[0]), int(rect[1])), (int(rect[0]) + int(rect[2]), int(rect[1]) + int(rect[3])), (255,255,255), 2)
-
-    img_color = img.get_labeled_image()
-    img_color = cv2.resize(img_color, (1024, 440))
-
-    img_concat = np.concatenate((img_color, img_disp), axis=1)
-    cv2.imshow("DTLD_visualized", img_concat)
-    cv2.waitKey(1)
+    # Visualize image by image
+    for idx_d, img in enumerate(database.images):
+        # Get disparity image
+        img_disp = img.visualize_disparity_image()
+        rects = img.map_labels_to_disparity_image(calibration)
+        # Plot labels into disparity image
+        for rect in rects:
+            cv2.rectangle(img_disp, (int(rect[0]), int(rect[1])), (int(rect[0]) + int(rect[2]), int(rect[1]) + int(rect[3])), (255,255,255), 2)
+        # Get color image with labels
+        img_color = img.get_labeled_image()
+        img_color = cv2.resize(img_color, (1024, 440))
+        # Plot side by side
+        img_concat = np.concatenate((img_color, img_disp), axis=1)
+        cv2.imshow("DTLD_visualized", img_concat)
+        cv2.waitKey(1)
 
 if __name__ == '__main__':
   main(parse_args())
