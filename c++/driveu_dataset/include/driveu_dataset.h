b571c19f8c5493bf65722729b3978ea08cb60428
Julian Mueller <muelju3@cmtcde8u013.rd.corpintra.net>
2020-04-29 22:08:25 +0200

    add documentation, make methods const if possible

--- c++/driveu_dataset/include/driveu_dataset.h
+++ c++/driveu_dataset/include/driveu_dataset.h
@@ -17,8 +17,9 @@ struct DriveuAttribute
 
     DriveuAttribute() : m_name_(""), m_class_(""){};
     DriveuAttribute(const std::string &n, const std::string &c) : m_name_(n), m_class_(c){};
-
+    /// Attribute name [e.g. "state"]
     std::string m_name_;
+    /// Attribute class [e.g. "red"]
     std::string m_class_;
 };
 
@@ -26,32 +27,56 @@ class DriveuObject
 {
 
 public:
+    /// Bounding box upper left x coordinate
     int m_x_;
+    /// Bounding box upper left y coordinate
     int m_y_;
+    /// Bounding box width
     int m_width_;
+    /// Bounding box height
     int m_height_;
+    /// Bounding box unique identity value
     int m_unique_id_;
+    /// Bounding box track identity (same real world instances in different images)
     std::string m_track_id_;
 
+    /// Label direction, can be ["front", "right", "left", "back"]
     DriveuAttribute m_direction_;
+    /// Label relevance, can be ["relevant", "not_relevant"]
     DriveuAttribute m_relevance_;
+    /// Label occlusion, can be ["occluded", "not_occluded"]. Occlusion includes truncation
     DriveuAttribute m_occlusion_;
+    /// Label orientation, can be ["vertical", "horizontal"]
     DriveuAttribute m_orientation_;
+    /// Label aspects, can be ["one_aspect", "two_aspects", "three_aspects", "four_aspects", "unknown"]
     DriveuAttribute m_aspects_;
+    /// Label states, can be ["red", "green", "yellow", "red_yellow", "off", "unknown"]
     DriveuAttribute m_state_;
+    /// Label pictograms, can be ["circle", "arrow_straight", "arrow_left", "arrow_right", "pedestrian", "bicycle", "tram", "arrow_straight_left", "unknown"]
     DriveuAttribute m_pictogram_;
-
-
 
     DriveuObject() : m_x_(0), m_y_(0), m_width_(0), m_height_(0), m_unique_id_(0), m_track_id_(""), m_direction_("direction", "unknown"), m_relevance_("relevance", "unknown"), m_occlusion_("occlusion", "unknown"), m_orientation_("orientation", "unknown"), m_aspects_("aspects", "unknown"), m_state_("state", "unknown"), m_pictogram_("pictogram", "unknown"){};
 
     DriveuObject(const int x, const int y, const int width, const int height, const int uid, const std::string &track_id, const std::string &direction, const std::string &relevance, const std::string &occlusion, const std::string &orientation, const std::string &aspects, const std::string &state, const std::string &pictogram) : m_x_(x), m_y_(y), m_width_(width), m_height_(height), m_unique_id_(uid), m_track_id_(track_id), m_direction_("direction", direction), m_relevance_("relevance", relevance), m_occlusion_("occlusion", occlusion), m_orientation_("orientation", orientation), m_aspects_("aspects", aspects), m_state_("state", state), m_pictogram_("pictogram", pictogram){};
 
+    /**
+     * @brief               Parses label dict from json file
+     * @param object_dict   Object dictionary read from json file
+     * @returns             Success
+     * */
     bool parseObjectDict(const Json::Value &object_dict);
 
 #ifdef OpenCV_FOUND
-    cv::Rect getRect();
-    cv::Scalar colorFromAttribute();
+    /**
+     * @brief               Returns label bbox as cv::Rect
+     * @returns             Label bbox (cv::Rect)
+     * */
+    cv::Rect getRect() const;
+    /**
+     * @brief               Returns color for bounding box based on state
+     * @returns             Label state as 3 channel color for visualization
+     * */
+    cv::Scalar colorFromAttribute() const;
 #endif
 };
 
@@ -59,27 +84,65 @@ class DriveuImage
 {
 
 public:
-    DriveuImage();
+    DriveuImage() : m_file_path_(""), m_disp_file_path_(""), m_timestamp_(0.), m_vehicle_data_(VehicleData()), m_objects_({})
+    {
+        std::map<int, std::vector<int>> map;
+        map[1023] = {1023, 1};
+        map[2559] = {4095, 2};
+        map[3455] = {32767, 32};
+        map[3967] = {65535, 64};
+        m_decomp_ = Decompand(map);
+    };
 
+    /// File path of left color image
     std::string m_file_path_;
+    /// File path of disparity image
     std::string m_disp_file_path_;
-
+    /// Timestamp of frame
     double m_timestamp_;
+    /// Additional sensor data at current frame
     VehicleData m_vehicle_data_;
-
+    /// List of labels in frame
     std::vector<DriveuObject> m_objects_;
-
-    Decompand *m_decomp_;
-
+    /// Decompanding (needed for 12 -> 16 bit)
+    Decompand m_decomp_;
+    /**
+     * @brief       Parses image dictionary from json label file
+     * @returns     Success
+     * */
     bool parseImageDict(const Json::Value &image_dict);
 
 #ifdef OpenCV_FOUND
-    bool getImage(cv::Mat &imageMat);
+    /**
+     * @brief       Returning 8 bit left color image (BGR channel order)
+     * @returns     cv::Mat
+     * */
+    cv::Mat getImage() const;
+    /**
+     * @brief    Returning 16 bit left color image (BGR channel order)
+     * @returns  cv::Mat
+     * */
     cv::Mat getImage16Bit();
-    bool getDisparityImage(cv::Mat &dispMat);
-    bool getLabeledImage(cv::Mat &imageMat);
-    void visualizeDisparityImage(cv::Mat &dispMat);
-    std::vector<cv::Rect> mapLabelsToDisparityImage(CalibrationData &calib);
+    /**
+     * @brief   return disparity image as float, where each pixel contains the disparity in pixels. Please note, that the disparity image has a smaller resolution than the original image and for the lower 144 pixels no disparity values are available
+     * @returns cv::Mat 32FC1
+     * */
+    cv::Mat getDisparityImage() const;
+    /**
+     * @brief   returns camera image with visualized bounding boxes colorized by their labeled state
+     * @returns cv::Mat in BGR channel order
+     * */
+    cv::Mat getLabeledImage() const;
+    /**
+     * @brief   Colorizes a 1 channel disparity image and returns it as a 3 channel image. Please note, that the pixel values cannot be used for 3D reconstruction anymore, use getDisparityImage() for this purpose!
+     * @returns cv::Mat in 3 channelBGR channel order
+     * */
+    void visualizeDisparityImage(cv::Mat &dispMat) const;
+    /**
+     * @brief       Returns all labels of the acutal image in disparity image           coordinates. Original label coordinates are first rectified and then mapped via binning factors
+     * @param calib Calibration of left camera
+     * */
+    std::vector<cv::Rect> mapLabelsToDisparityImage(CalibrationData &calib) const;
 #endif
 };
 
@@ -88,7 +151,12 @@ class DriveuDatabase
 
 public:
     DriveuDatabase();
-
+    // all images of the dataset
     std::vector<DriveuImage> m_images_;
+    /**
+     * @brief           Loads database from .json labelfile
+     * @param path      Label file path
+     * @param base_path Optional: If dataset images directory differs from the label files (e.g. moved to another directory) this can be set here. Please note, that the base path refers to the top directory of the original dataset structure.
+     * */
     bool open(const std::string &path, const std::string &base_path);
 };
--- c++/driveu_dataset/src/driveu_dataset.cpp
+++ c++/driveu_dataset/src/driveu_dataset.cpp
@@ -8,19 +8,6 @@
     @brief      Contains class definitions and methods to parse the DriveU Database: Label format, load images, load disparity images and load calibration data. Either with or without OpenCV.
 */
 
-/* ##################### DRIVEU OBJECT (LABEL) ##################### */
-
-/**
- * @brief DriveuDatabase::open  Open the DriveU Database from file
- * @param m_x_         upper left corner
- * @param m_y_         upper left corner
- * @param width     width of label
- * @param height    height of label
- * @param unique_id Every TL has a unique ID
- * @param track_id  Track ID of the TL. Each instance of a traffic light has the same track id until the vehicle passed this instance. Vehicle passed the instance when the sequence changes. Detect sequence changes when sequence name (e.g. /2015-04-21_17-09-21/") in file path changes. Actually we use string notation, e.g. TrafficLight_1, TrafficLight_2 ...
-*/
-
-
 
 bool DriveuObject::parseObjectDict(const Json::Value &object_dict)
 {
@@ -49,23 +36,6 @@ bool DriveuObject::parseObjectDict(const Json::Value &object_dict)
 
 /* ##################### DRIVEU IMAGE ##################### */
 
-/**
- * @brief DriveuImage::DriveuImage Constructor
- */
-DriveuImage::DriveuImage()
-{
-
-    m_file_path_ = "";
-    m_disp_file_path_ = "";
-    m_timestamp_ = 0.0;
-    m_objects_.clear();
-    std::map<int, std::vector<int>> map;
-    map[1023] = {1023, 1};
-    map[2559] = {4095, 2};
-    map[3455] = {32767, 32};
-    map[3967] = {65535, 64};
-    m_decomp_ = new Decompand(map);
-}
 
 bool DriveuImage::parseImageDict(const Json::Value &image_dict)
 {
@@ -127,17 +97,7 @@ bool DriveuDatabase::open(const std::string &path, const std::string &base_path)
     return true;
 }
 
-
-
-/**
- * @brief DriveuObject::colorFromAttribute    Returns color from classID of one object
- * DIGIT 1 : 0 = not relevant, 1 = relevant TL, 4 = occluded
- * DIGIT 2 : 1 = horizontal TL, 2 = vertical TL, 3 = horizontal without frame, 4 = vertical without frame, 6 = horizontal Bus/Tram TL, 7 = vertical bus/tram, 8 = hor. without frame, 9 = vert. without frame
- * DIGIT 3 : 1 = single light TL, 2 = dual light TL, 3 = triple light TL (most common), ...
- * DIGIT 4 : 0 = light off, 1 = red, 2 = yellow, 3 = red-yellow, 4 = green, 5 = white, ...
- * DIGIT 5 : 1 = no light mask, 2 = arrow straight, 3 = arrow left + straight, 4 = arrow right, 5 = arrow right + straight, 7 = rttow left + right + straight, 8 = pedestrian mask in light, 9 = bike mask in light
- */
-cv::Scalar DriveuObject::colorFromAttribute()
+cv::Scalar DriveuObject::colorFromAttribute() const
 {
 
     if (m_state_.m_class_ == "red")
@@ -162,62 +122,43 @@ cv::Scalar DriveuObject::colorFromAttribute()
     }
 }
 
-/**
- * @brief DriveuObject::getRect()   Return object bounding box as cv Rect (upper left x, upper left y, width, height), x = horizontal axis, y = vertical axis
- * */
-cv::Rect DriveuObject::getRect()
+
+cv::Rect DriveuObject::getRect() const
 {
     return cv::Rect(m_x_, m_y_, m_width_, m_height_);
 }
 
-/**
- * @brief DriveuImage::getImage()  Return image in 8 Bit as BGR. Raw image is loaded from file, debayered and shifted from 12 bit to 8 bit
- * @param imageMat  Color image in 8 bit
- * */
-bool DriveuImage::getImage(cv::Mat &imageMat)
+cv::Mat DriveuImage::getImage() const
 {
 
     if (std::fstream(m_file_path_))
     {
-
-        imageMat = cv::imread(m_file_path_, -1);
+        cv::Mat imageMat = cv::imread(m_file_path_, -1);
         cv::cvtColor(imageMat, imageMat, CV_BayerGB2BGR);
         imageMat.convertTo(imageMat, CV_MAKETYPE(CV_8U, imageMat.channels()), 1. / (1 << (12 - 8)));
-        return true;
+        return imageMat;
     }
     else
     {
-        std::cerr << "driveuDatabase:\tERROR: File " << m_file_path_ << " not found!" << std::endl
-                  << std::endl;
-        return false;
+        std::cerr << "driveuDatabase:\tERROR: File " << m_file_path_ << " not found!" << std::endl;
+        return cv::Mat();
     }
 }
 
-/**
- * @brief DriveuImage::getImage16Bit()  Return image in 16 Bit as BGR. Raw image is loaded from file and corrected with help of sensor characteristic
- * @param imageMat  Color image in 16 bit
- * */
 cv::Mat DriveuImage::getImage16Bit()
 {
     cv::Mat imageMat = cv::imread(m_file_path_, -1);
 
     cv::Mat linear;
 
-    if (m_decomp_ == NULL)
-    {
-        linear = imageMat;
-    }
-    else
-    {
-        // decompand in place
-        linear = imageMat.clone();
-        unsigned short *dst = reinterpret_cast<unsigned short *>(linear.data);
-        unsigned short *src = reinterpret_cast<unsigned short *>(imageMat.data);
+    // decompand in place
+    linear = imageMat.clone();
+    unsigned short *dst = reinterpret_cast<unsigned short *>(linear.data);
+    unsigned short *src = reinterpret_cast<unsigned short *>(imageMat.data);
 
-        for (size_t p = 0; p < size_t(imageMat.rows) * size_t(imageMat.cols); ++p)
-        {
-            m_decomp_->processPixel(src[p], dst[p]);
-        }
+    for (size_t p = 0; p < size_t(imageMat.rows) * size_t(imageMat.cols); ++p)
+    {
+        m_decomp_.processPixel(src[p], dst[p]);
     }
 
     cv::cvtColor(linear, linear, CV_BayerGB2BGR);
@@ -226,18 +167,14 @@ cv::Mat DriveuImage::getImage16Bit()
     return linear;
 }
 
-/**
- * @brief DriveuImage::getDisparityImage() return disparity image as float, where each pixel contains the disparity in pixels. Please note, that the disparity image has a smaller resolution than the original image
- * and for the lower 144 pixels no disparity values are available
- * */
-bool DriveuImage::getDisparityImage(cv::Mat &dispMat)
+cv::Mat DriveuImage::getDisparityImage() const
 {
 
     if (std::fstream(m_disp_file_path_))
     {
 
         cv::Mat imageMat = cv::imread(m_disp_file_path_, -1);
-        dispMat = cv::Mat(imageMat.rows, imageMat.cols, CV_32F, cv::Scalar(0.));
+        cv::Mat dispMat = cv::Mat(imageMat.rows, imageMat.cols, CV_32F, cv::Scalar(0.));
 
         float scale_ = 1. / (1 << 4);
         const unsigned short *inImg = reinterpret_cast<const unsigned short *>(&imageMat.data[0]);
@@ -258,14 +195,12 @@ bool DriveuImage::getDisparityImage(cv::Mat &dispMat)
             }
         }
 
-        return true;
+        return imageMat;
     }
-
     else
     {
-        std::cerr << "driveuDatabase:\tERROR: File " << m_disp_file_path_ << " not found!" << std::endl
-                  << std::endl;
-        return false;
+        std::cerr << "driveuDatabase:\tERROR: File " << m_disp_file_path_ << " not found!" << std::endl;
+        return cv::Mat();
     }
 }
 
@@ -273,7 +208,7 @@ bool DriveuImage::getDisparityImage(cv::Mat &dispMat)
  * @brief DriveuImage::mapLabelsToDisparityImage() Returns all labels of the acutal image in disparity image coordinates. Original label coordinates are first rectified and then mapped via binning factors
  * @param calib Calibration of left camera
  * */
-std::vector<cv::Rect> DriveuImage::mapLabelsToDisparityImage(CalibrationData &calib)
+std::vector<cv::Rect> DriveuImage::mapLabelsToDisparityImage(CalibrationData &calib) const
 {
 
     std::vector<cv::Rect> rects_disparity;
@@ -301,10 +236,8 @@ std::vector<cv::Rect> DriveuImage::mapLabelsToDisparityImage(CalibrationData &ca
 
 #ifdef OpenCV_FOUND
 
-/**
- * @brief DriveuImage::visualizeDisparityImage() Modifies the disparity image and returns an CV_8UC3 image. Please note, that the pixel values cannot be used for 3D reconstruction anymore, use getDisparityImage() for this purpose!
- * */
-void DriveuImage::visualizeDisparityImage(cv::Mat &dispMat)
+
+void DriveuImage::visualizeDisparityImage(cv::Mat &dispMat) const
 {
 
     double min, max;
@@ -313,26 +246,20 @@ void DriveuImage::visualizeDisparityImage(cv::Mat &dispMat)
     cv::applyColorMap(dispMat, dispMat, cv::COLORMAP_JET);
 }
 
-/**
- * @brief DriveuImage::getLabeledImage() Returns the actual image together with all labeled objects in this frame drawn as a bounding rectangle in the color of the actual TL state.
- * */
-bool DriveuImage::getLabeledImage(cv::Mat &imageMat)
+cv::Mat DriveuImage::getLabeledImage() const
 {
-
-    if (getImage(imageMat))
+    cv::Mat imageMat = getImage();
+    if (!imageMat.empty())
     {
 
         for (size_t i = 0; i < m_objects_.size(); ++i)
         {
             cv::rectangle(imageMat, m_objects_[i].getRect(), m_objects_[i].colorFromAttribute(), 2);
         }
-        return true;
+        return imageMat;
     }
 
-    else
-    {
-        return false;
-    }
+    return cv::Mat();
 }
 
 #endif
